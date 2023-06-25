b5762c362f27dcd1a518d567991f525068ce9820
KhomenokNastya <khomenok.nastya@bk.ru>
2023-06-25 21:37:53 +0300

    Basic project setup, Mongo database connection

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModel } from '../user/user.model'
+import { ConfigModule } from '@nestjs/config'
+
+@Module({
+  controllers: [AuthController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: UserModel,
+        schemaOptions: {
+          collection: 'User'
+        }
+      }
+    ]),
+    ConfigModule,
+  ],
+  providers: [AuthService],
+})
+export class AuthModule {}
