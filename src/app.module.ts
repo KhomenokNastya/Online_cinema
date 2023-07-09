379ab0f772f104434f5ad1a9da081833bddc1d29
KhomenokNastya <khomenok.nastya@bk.ru>
2023-07-09 10:10:48 +0500

    Add rating

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypegooseModule } from 'nestjs-typegoose'
-import { getMongoDbConfig } from './config/mongo.config'
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoDbConfig } from './config/mongo.config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GenreModule } from './genre/genre.module';
 import { FileModule } from './file/file.module';
 import { ActorModule } from './actor/actor.module';
 import { MovieModule } from './movie/movie.module';
+import { RatingModule } from './rating/rating.module';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { MovieModule } from './movie/movie.module';
     TypegooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: getMongoDbConfig
+      useFactory: getMongoDbConfig,
     }),
     AuthModule,
     UserModule,
@@ -25,6 +26,7 @@ import { MovieModule } from './movie/movie.module';
     FileModule,
     ActorModule,
     MovieModule,
+    RatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,21 +1,24 @@
-import { Module } from '@nestjs/common';
-import { MovieController } from './movie.controller';
-import { MovieService } from './movie.service';
+import { Module } from '@nestjs/common'
 import { TypegooseModule } from 'nestjs-typegoose'
+import { UserModule } from 'src/user/user.module'
+import { MovieController } from './movie.controller'
 import { MovieModel } from './movie.model'
+import { MovieService } from './movie.service'
 
 @Module({
+  controllers: [MovieController],
   imports: [
     TypegooseModule.forFeature([
       {
         typegooseClass: MovieModel,
         schemaOptions: {
-          collection: 'Movie'
+          collection: 'Movie',
         },
       },
     ]),
+    UserModule,
   ],
-  controllers: [MovieController],
-  providers: [MovieService]
+  providers: [MovieService],
+  exports: [MovieService],
 })
 export class MovieModule {}
